5ac899e322442540921144faacfa4383dd0fdb9b
AWDEV CORPORATION <84139744+wahyu9kdl@users.noreply.github.com>
2022-06-17 08:07:57 +0700

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
-          className="App-link"
+          className="Awdev-Editor"
           href="https://www.awdev.my.id/home.html"
           target="_blank"
           rel="noopener noreferrer"
