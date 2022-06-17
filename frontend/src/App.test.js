2a099116ebce7032af182035c9463bede889bf36
AWDEV CORPORATION <84139744+wahyu9kdl@users.noreply.github.com>
2022-06-17 07:54:09 +0700

    Update App.test.js

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from 'git@github.com:wahyu9kdl/wahyu9kdl.github.io.git';
+import App from './wahyu9kdl.github.io';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/wahyu9kdl.github.io/DASHBOARD/TOOLS/CodeSandbox.html);
   expect(linkElement).toBeInTheDocument();
 });
